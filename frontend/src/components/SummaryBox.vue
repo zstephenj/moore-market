b86a411a057e6e771a5e84e5bebe1b5c2759a83e
Clarence Coppinger <thea.trio@gmail.com>
2019-02-04 14:11:49 -0600

    merge from upstream

---
+++ frontend/src/components/SummaryBox.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class=summaryBox>
+      <h2>Summary</h2>
+      <hr/>
+      <ul>
+          <li v-for="message in sumBoxMessages">{{ message }}</li>
+      </ul>
+    </div>
+</template>
+
+<script>
+export default {
+   name: 'SummaryBox',
+   props: {
+    sumBoxMessages: {
+        type: Array
+    }
+   },
+   data() {
+       return {
+         
+
+       }
+       
+       
+   } 
+}
+</script>
+
+<style scoped>
+.summaryBox {
+    float: right;
+    border: 3px solid seagreen;
+    padding: 10px;
+    width: 300px;
+    margin: 15px;
+}
+.summaryBox ul {
+    text-align: left;
+}
+</style>
+
