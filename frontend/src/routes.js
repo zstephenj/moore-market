f4399ba84047f6c3c0e2995736e4d021998990fd
Irwin Tani <itani05@gmail.com>
2019-02-04 20:54:30 -0600

    Added Vue router, routes, Dashboard, UserDashboard placeholder

---
+++ frontend/src/routes.js
@@ -0,0 +1,7 @@
+import HelloWorld from './components/HelloWorld.vue';
+import Dashboard from './Dashboard.vue';
+
+export default [
+    { path: '/', component: HelloWorld},
+    { path: '/dashboard', component: Dashboard},
+]
