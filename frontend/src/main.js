8072c66a6f50a7001d0765feaf22f22e6e8908cf
Stephen Zhang <zhangstephenj@gmail.com>
2019-02-07 02:52:55 -0600

    Removed Bootstrap CDN, installed Bootstrap via npm

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,9 +1,14 @@
 import Vue from 'vue';
+
 import App from './App.vue';
 
 import store from './store/store';
 
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+
 Vue.config.productionTip = false
+
 
 new Vue({
   render: h => h(App),
