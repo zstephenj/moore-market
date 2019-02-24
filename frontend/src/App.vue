5d3cb58bba10c04353fd7b74cd7a2e30b243ee59
Irwin Tani <itani05@gmail.com>
2019-02-24 17:26:57 -0600

    pull products and farmers into user dashboard

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <NavigationTwo></NavigationTwo>
+    <Navigation></Navigation>
     <router-view></router-view>
   </div>
 </template>
--- frontend/src/components/UserDashboard.vue
+++ frontend/src/components/UserDashboard.vue
@@ -1,12 +1,42 @@
 <template>
     <div>
       <h1>This will be the User Dashboard</h1>
+      <div>
+        <h4>Product List</h4>
+        <ul>
+          <li v-for="product in allProducts" :key="product.id">{{ product.name }}</li>
+        </ul>
+      </div>
+      <div> 
+        <h4>Farmer List</h4>
+        <ul>
+          <li v-for="farmer in allFarmers" :key="farmer.id">{{ farmer.username }}</li>
+        </ul>
+      </div>
     </div>
 </template>
 
 <script>
+import { mapActions, mapState } from 'vuex'
+
 export default {
-    
-}
+  name: 'userdashbaord',
+  components: {
+      
+  },
+  created() {
+    this.getFarmers(),
+    this.getAllProductsFromApi()
+  },
+  computed: {
+    ...mapState('product', ['allProducts']),
+    ...mapState('farmer', ['allFarmers'])
+  },
+  methods: {
+    ...mapActions('product', ['getAllProductsFromApi']),
+    ...mapActions('farmer', ['getFarmers'])
+  }  
+
+}  
 </script>
 
