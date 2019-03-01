75795a0504cd47f286cde63c7b8c18ad2290429a
Stephen Zhang <zhangstephenj@gmail.com>
2019-02-28 18:07:20 -0600

    Create Product Category Form

---
+++ frontend/src/components/Product/ProductCategoryForm.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class='container-fluid'>
+        <form v-on:submit.prevent="onSubmit">
+            <div class="form-group-sm">
+                <input v-model='categoryName' @input='sendCategory()' type="text" class="form-control form-control-sm" id="CategoryName" placeholder="New Category Name">
+            </div>
+        </form>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name:'ProductCategoryForm',
+    props: ['formType', 'oldCategoryName'],
+    data() {
+        return {
+            categoryName: ''
+        }
+    },
+
+    methods: {
+        sendCategory(){
+            this.$emit('category-input', this.categoryName)
+        }
+    },
+    created() {
+        this.categoryName = this.oldCategoryName
+    }
+}
+</script>
+
+<style>
+
+</style>
