138ad20bb7d0584fd188e0f5a6ca6aa8f321481f
Stephen Zhang <zstephenj@gmail.com>
2019-03-01 01:57:27 -0600

    Add store functions for location property of user module state

--- frontend/src/store/modules/user.js
+++ frontend/src/store/modules/user.js
@@ -11,14 +11,81 @@ const user = {
     },
 
     mutations: {
+
+        setCurrentUser (state, payload) {
+            state.currentUser = payload
+        },
+
+        pushLocation (state, payload) {
+            state.currentUser.location.push(payload)
+        },
+
+        removeLocation (state, payload) {
+            state.currentUser.splice(payload, 1)
+        },
+
     },
 
     actions: {
+
+        async getUserFromApi({ commit }, login) {
+            let response
+            try {
+                // Comment next line if using backend database
+                // response = await axios.get('https://my-json-server.typicode.com/zstephenj/moore-market-fakeJSON-categories/categories', login)
+                // Comment next line if using FakeJSON
+                // response = await axios.get('/api/categories')
+                // commit('setCurrentUser', response.data)
+                return response
+            } catch (error) {
+                console.error(error)
+            }
+
+        },
+
+        async addLocation({ commit }, location) {
+            let response
+            try {
+                // Comment next line if using backend database
+                // response = await axios.post('http://my-json-server.typicode.com/zstephenj/moore-market-fakeJSON-categories/categories', location)
+                // Comment next line if using FakeJSON
+                // response = await axios.post('/api/categories/add')
+                commit('pushLocation', location)//response.data)
+                return response
+            } catch (error) {
+                console.error(error)
+            }
+
+        },
+
+        async removeLocation({ commit, getters }, location) {
+            let response
+            try {
+                // Comment next line if using backend database
+                // response = await axios.delete('http://my-json-server.typicode.com/zstephenj/moore-market-fakeJSON-categories/categories/' + id)
+                // Comment next line if using FakeJSON
+                // response = await axios.delete('/api/categories/remove/' + id)
+                let idx = getters.getLocationIndex(location)
+                commit('removeLocation', idx)
+                return response
+            } catch (error) {
+                console.error(error)
+            }
+
+        },
+
+
     },
 
     getters: {
-    },
-    
+
+        getLocationIndex: (state) => (location) => {
+            console.log(state.location)
+            return state.location.findIndex(location)
+        },
+
+    }
+
 }
 
 export default user
