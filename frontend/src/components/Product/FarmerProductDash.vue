c28f919b2ea08ac9f74722390a21c18de27b62a7
Clarence Coppinger <thea.trio@gmail.com>
2019-03-02 11:36:02 -0600

    Fixed the farmer dashboard

--- frontend/src/components/Product/FarmerProductDash.vue
+++ frontend/src/components/Product/FarmerProductDash.vue
@@ -12,7 +12,7 @@
                 </tr>
             </thead>
             <tbody>
-                <farmer-product-item v-for='product in farmerProducts' :key='product.id' :product='product'> </farmer-product-item>
+                <farmer-product-item v-for='product in farmerProducts.products' :key='product.id' :product='product'> </farmer-product-item>
             </tbody>
         </table>
     </div>
@@ -50,7 +50,8 @@ export default {
 
     created() {
         // Fills store state with data from fakeJSON API rather than calling store actions to fill state.Product.AllProducts
-        this.getFarmerProducts(this.id)       
+        this.getFarmerProducts(this.id)
+        console.log(this.farmerProducts)      
     },
 }
 </script>
