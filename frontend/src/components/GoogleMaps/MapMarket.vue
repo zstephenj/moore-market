2602953d87c3898e854dab599313de17f6d5042a
Stephen Zhang <zstephenj@gmail.com>
2019-03-01 20:40:55 -0600

    Create MapMarket

---
+++ frontend/src/components/GoogleMaps/MapMarket.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class='container-fluid border-navy'>
+
+        <div class='my-2'>
+            <h2>{{market.name}}</h2>
+        </div>
+
+        <hr class='border-navy' />
+        <div class='d-flex align-items-stretch'></div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'MapMarket',
+    props: ['market'],
+
+}
+</script>
+
+<style scoped>
+
+.border-navy {
+    border-style:solid;
+    border-color: #001f3f;
+}
+
+</style>
