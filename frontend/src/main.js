abe8ba6e1151a20a8b2b1ee08d0643654d1c702a
Stephen Zhang <zstephenj@gmail.com>
2019-03-05 02:31:22 -0600

    import jquery, bootstrap js (popper.js included)

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -6,9 +6,11 @@ import router from './router'
 import store from './store';
 
 import geo from './geo'
+
+import 'jquery/dist/jquery.slim.js'
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
-
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 
 Vue.config.productionTip = false
 
